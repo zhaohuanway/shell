bd4021455295e80dd5e8e4d90586c7d166aae6d4
奥力给 <51152134+rourouaa@users.noreply.github.com>
2023-08-19 17:33:40 +0800

    Update nginx日志切割.sh

--- nginx日志切割.sh
+++ nginx日志切割.sh
@@ -2,6 +2,7 @@
 date=`date +%Y%m%d`
 logpath=/usr/local/nginx/logs
 mv $logpath/error.log $logpath/error-$date.log 
+#
 mv $logpath/access.log $logpath/access-$date.log
 kill -USR1 $(cat $logpath/nginx.pid)       # $(cat $logpath/nginx.pid)相当与`cat $logpath/nginx.pid` 
 
